1571aae7833807ea9da6b43eb61156a1aca8cfc3
Lance Klinger <lance.d.klinger@gmail.com>
2015-03-15 22:29:51 -0400

    updated index html to use django template system so that the template can be reused on all pages throughout the site

--- django/gitpyt/urls.py
+++ django/gitpyt/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-# from gitpyt import views
-
 urlpatterns = patterns('',
     url(r'^$', 'gitpyt.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
--- django/gitpyt/views.py
+++ django/gitpyt/views.py
@@ -3,9 +3,5 @@ from django.template import RequestContext, loader
 from django.shortcuts import render
 
 def index(request):
-	# template = loader.get_template('gitpyt/index.html')
- 	#context = RequestContext(request)
- 	print("Index")
 
- 	# return HttpResponse("Hello gitpy")
  	return render(request, "gitpyt/index.html")
