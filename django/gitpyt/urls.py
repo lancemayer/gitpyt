dbc6f04a62b5966d8b1c9f127356506accb82a16
Lance Klinger <lance.d.klinger@gmail.com>
2015-03-23 15:43:07 +0000

    testing ability to create git bare repo. minimally successful: dir ownership issues with using one user to run web server and another (git) for hosting git repos

--- django/gitpyt/urls.py
+++ django/gitpyt/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'gitpyt.views.index', name='index'),
+    url(r'^mkdir/$', 'gitpyt.views.mkdir', name='mkdir'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- django/gitpyt/views.py
+++ django/gitpyt/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+import os
 
 def index(request):
 
  	return render(request, "gitpyt/index.html")
+
+def mkdir(request):
+	os.system("git init --bare /home/git/proj.git")
+
+	return render(request, "gitpyt/mkdir.html")
