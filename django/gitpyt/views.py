d8aa8c337393fd81f2ca406b6573c632e5bb4b98
Lance Klinger <lance.d.klinger@gmail.com>
2015-03-23 19:09:22 +0000

    modified git repo creation to determine current user

--- django/gitpyt/views.py
+++ django/gitpyt/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
-import os
+import os, getpass
 
 def index(request):
 
  	return render(request, "gitpyt/index.html")
 
 def mkdir(request):
-	os.system("git init --bare /home/git/proj.git")
+	initPath = "git init --bare /home/" + getpass.getuser() + "/proj2.git"
+	# os.system("git init --bare /home/git/proj.git")
+	os.system(initPath)
+	# print(getpass.getuser())
 
 	return render(request, "gitpyt/mkdir.html")
