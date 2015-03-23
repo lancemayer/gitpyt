46c8a8509eac1b725477676c8e4462edd5227511
Lance Klinger <lance.d.klinger@gmail.com>
2015-03-23 19:13:37 +0000

    replace /home/user with ~

--- django/gitpyt/views.py
+++ django/gitpyt/views.py
@@ -8,7 +8,8 @@ def index(request):
  	return render(request, "gitpyt/index.html")
 
 def mkdir(request):
-	initPath = "git init --bare /home/" + getpass.getuser() + "/proj2.git"
+	initPath = "git init --bare ~/proj3.git"
+	# initPath = "git init --bare /home/" + getpass.getuser() + "/proj2.git"
 	# os.system("git init --bare /home/git/proj.git")
 	os.system(initPath)
 	# print(getpass.getuser())
