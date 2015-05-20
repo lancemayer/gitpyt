de3b13ddc339ae79fb71091b5dcf8b4fa5fc2cbb
Lance Klinger <lance.d.klinger@gmail.com>
2015-05-19 23:13:38 -0400

    Improved for appearance with bootstrap theme class. Updated urls list format.

--- django/gitpyt/urls.py
+++ django/gitpyt/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', 'gitpyt.views.index', name='index'),
     url(r'^mkdir/$', 'gitpyt.views.mkdir', name='mkdir'),
     url(r'^createrepository/', include('createrepository.urls', namespace="createrepository")),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
