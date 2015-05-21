4440e6405e69c1028102a6d7754f3154266fcb41
Lance Klinger <lance.d.klinger@gmail.com>
2015-05-20 22:56:25 -0400

    Added signup and login
    User can sign up, login, and logout.
    Process needs cleaned up and user directory for repos needs created when user signs up.

--- django/gitpyt/urls.py
+++ django/gitpyt/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'gitpyt.views.index', name='index'),
-    url(r'^mkdir/$', 'gitpyt.views.mkdir', name='mkdir'),
-    url(r'^createrepository/', include('createrepository.urls', namespace="createrepository")),
+    url(r'^login/$', 'gitpyt.views.loginPage', name='loginPage'),
+    url(r'^authenticate/$', 'gitpyt.views.authenticateUser', name='authenticateUser'),
+    url(r'^logout/$', 'gitpyt.views.logoutUser', name='logoutUser'),
+    url(r'^signup/$', 'gitpyt.views.signup', name='signup'),
+    url(r'^createrepository/', include('createrepository.urls', namespace='createrepository')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- django/gitpyt/views.py
+++ django/gitpyt/views.py
@@ -1,17 +1,51 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 import os, getpass
 
 def index(request):
 
  	return render(request, "gitpyt/index.html")
 
-def mkdir(request):
-	home = "~"
-	projectName = "proj4.git"
-	initPath = "git init --bare " + home + "/" + projectName
-	# os.system(initPath)
-	# print(getpass.getuser())
+def loginPage(request):
 
-	return render(request, "gitpyt/mkdir.html")
+	return render(request, "gitpyt/login.html")
+
+def authenticateUser(request):
+	if request.method == "POST":
+		if request.user.is_authenticated() == False:
+			return authenticateAndLoginUserInternal(request)
+		else:
+			#already logged in
+			return redirect('/')
+
+def authenticateAndLoginUserInternal(request):
+	username = request.POST["username"]
+	password = request.POST["password"]
+	user = authenticate(username = username, password = password)
+	if user is not None:
+		login(request, user)
+		return redirect('/')
+	else:
+		return HttpResponse("login failed")
+
+def logoutUser(request):
+	logout(request)
+	
+	return redirect('/')
+
+def signup(request):
+	if request.method == 'POST':
+		firstName = request.POST["firstName"]
+		lastName = request.POST["lastName"]
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = User.objects.create_user(username = username, password = password, first_name = firstName, last_name = lastName)
+
+		user.save()
+
+		return render(request, "gitpyt/login.html")
+
+	return render(request, "gitpyt/signup.html")
